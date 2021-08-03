2def1b9f14873f68fcec8a98244561224ed91611
Z3V3L <zevelbs@gmail.com>
2021-08-02 19:51:54 -0500

    main three changes

--- Threejs/vite-project/main.js
+++ Threejs/vite-project/main.js
@@ -1,48 +1,72 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
 import * as THREE from 'three';
-import { ColladaLoader } from 'three';
+
+import { ColladaLoader } from 'three/examples/jsm/loaders/ColladaLoader';
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-            renderer.setSize( window.innerWidth, window.innerHeight );
-            document.body.appendChild( renderer.domElement );
+//adding Grid helper
 
-            const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-            camera.position.set( 0, 0, 50 );
-            camera.lookAt( 0, 0, 0 );
+const size = 20;
+const divisions = 150;
 
-            const scene = new THREE.Scene();
+const gridHelper = new THREE.GridHelper( size, divisions );
+scene.add( gridHelper );
+
+/*---- Adding a cube ----*/
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material);
+
+//scene.add( cube );
+cube.position.y = 0.5;
+
+// Move camera off the cube in (0, 1, 5).
+camera.position.x = 4;
+camera.position.y = 1;
+camera.position.z = 10;
+
+/*const loader = new ColladaLoader();
+// loader.load( './models/collada/kawada-hironx.dae', function ( collada ) {
+loader.load( 'Handgun_dae.dae', function ( collada ) {
+
+    scene.add( collada.scene );
+
+}, undefined, function ( error ) {
+
+    console.error( error );
+
+} ); */
+
+const loader = new GLTFLoader();
+
+loader.load( 'untitled.glb', function ( gltf ) {
+
+    scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+    console.error( error );
+
+} );
 
 
-            const material = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
-            const points = [];
-            points.push( new THREE.Vector3(-10, 0, 0 ) );
-            points.push( new THREE.Vector3(0, 10, 0 ) );
-            points.push( new THREE.Vector3(10, 0, 0 ) );
+//Rendering scene
+function animate() {
+    /*requestAnimationFrame( animate );
 
-            const geometry = new THREE.BufferGeometry().setFromPoints( points );
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;*/
 
-            const line = new THREE.Line( geometry, material );
-
-            scene.add( line );
-
-
-            const loader = new ColladaLoader();
-
-            loader.load( 'Agumon [17]/Agumon.dae', function ( collada ) {
-
-              scene.add( collada.scene );
-
-            }, undefined, function ( error ) {
-
-              console.error( error );
-
-            } );
-
-            renderer.render( scene, camera);
+    renderer.render( scene, camera );
+}
+animate();
